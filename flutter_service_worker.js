db4225e11b19bb066c6aa30037026b481efd1355
Thant Myat Thiri <poenge.tmt@gmail.com>
2024-11-21 14:44:30 +0630

    add marker

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,15 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+const RESOURCES = {"assets/AssetManifest.bin": "5dca13607ffc66e59f3d8b1b0c11bd6e",
+"assets/AssetManifest.bin.json": "e1628ceba4f756a7548323f7bd80d44e",
+"assets/AssetManifest.json": "f58fa1f3a811cd686e46028dec169b4d",
+"assets/assets/bus_marker.png": "3af3afa30e7926c5d26c2be093621e8e",
+"assets/assets/destination_marker.png": "47ebba9d7489c6af4700bc68f00fd08f",
+"assets/assets/origin.png": "dbab5d8c4961be6075d6664cd505a69d",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "8ea08ea2444cc58ec5807fd7669e488f",
-"assets/NOTICES": "18056ce8a94bdf92a9e12a482e1a9680",
+"assets/NOTICES": "679e4bbebc28592c6c3b7f70620b8649",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
@@ -23,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "fc9f8087f9bcece205552673163c8c3b",
+"flutter_bootstrap.js": "5f2cbc3a569dad44bda6543fab042b8a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "dc15f2a7a6caacef963ac196e3f81f2a",
 "/": "dc15f2a7a6caacef963ac196e3f81f2a",
-"main.dart.js": "9393cddc3ee0dc38e0199f2a4bc11ab0",
+"main.dart.js": "9116f897ec2236ec66313b00638355e2",
 "manifest.json": "576a6afe5fc68e454a49be0dff0a5de6",
 "version.json": "88c4e44ff4237ce0ac5785b2de0ad6b4"};
 // The application shell files that are downloaded before a service worker can
